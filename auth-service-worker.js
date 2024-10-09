a29c698966dff582dfb75bbc512bf1eb0379c21b
William Taylor <wtaylor1984@gmail.com>
2024-10-08 21:48:31 -0700

    Revert "Show signin state"
    This reverts commit cffee57629125fe79fdc3c0513d3294c0b9edb6e.

--- auth-service-worker.js
+++ auth-service-worker.js
@@ -23,23 +23,12 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(fetchWithFirebaseHeaders(event.request));
 });
 
+// TODO: add Firebase Authentication headers to request
 async function fetchWithFirebaseHeaders(request) {
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  const installations = getInstallations(app);
-  const headers = new Headers(request.headers);
-  const [authIdToken, installationToken] = await Promise.all([
-    getAuthIdToken(auth),
-    getToken(installations),
-  ]);
-  headers.append("Firebase-Instance-ID-Token", installationToken);
-  if (authIdToken) headers.append("Authorization", `Bearer ${authIdToken}`);
-  const newRequest = new Request(request, { headers });
-  return await fetch(newRequest);
+  return await fetch(request);
 }
 
+// TODO: get user token
 async function getAuthIdToken(auth) {
-  await auth.authStateReady();
-  if (!auth.currentUser) return;
-  return await getIdToken(auth.currentUser);
+  throw new Error('not implemented');
 }
