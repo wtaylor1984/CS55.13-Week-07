528bce08cb399e7acd73fd94dc78b32cb9dea2c0
William Taylor <wtaylor1984@gmail.com>
2024-10-08 22:26:17 -0700

    rolling back to the very beginning

--- src/lib/firebase/auth.js
+++ src/lib/firebase/auth.js
@@ -7,23 +7,13 @@ import {
 import { auth } from "@/src/lib/firebase/clientApp";
 
 export function onAuthStateChanged(cb) {
-  return _onAuthStateChanged(auth, cb);
+  return () => {};
 }
 
 export async function signInWithGoogle() {
-const provider = new GoogleAuthProvider();
-
-try {
-await signInWithPopup(auth, provider);
-} catch (error) {
-console.error("Error signing in with Google", error);
-}
+  return;
 }
 
 export async function signOut() {
-try {
-return auth.signOut();
-} catch (error) {
-console.error("Error signing out with Google", error);
+  return;
 }
-}s
